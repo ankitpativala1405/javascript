1106581fb6787f29de9e18a4a8e713141a6209c0
Your Name <you@example.com>
2025-04-28 19:50:31 +0530

    update

--- e-com website/javascript/product.js
+++ e-com website/javascript/product.js
@@ -1,6 +1,8 @@
 import { CartMethod } from "../api/cartmethod.js";
 import { ProductMethod } from "../api/productmethod.js";
 import Navbar from "../components/navbar.js";
+import { SearchValue } from "../components/search.js";
+import uiMaker from "../components/uimakerproducts.js";
 import { isLoggedIn } from "../utils/helper.js";
 document.getElementById("navbar").innerHTML = Navbar();
 let logoutBtn = document.getElementById("logout-btn");
@@ -15,56 +17,40 @@ isLoggedIn();
   uiMaker(products);
 })();
 
-const uiMaker = (data) => {
-  data.map((product) => {
-    let div = document.createElement("div");
-    div.className = "card mb-4";
-    div.style.width = "18rem";
-    div.setAttribute("class", "container");
+//sort
+document.getElementById("SortByPrise").addEventListener("change", async () => {
+  let SortValue = document.getElementById("SortByPrise").value;
 
-    let img = document.createElement("img");
-    img.src = product.img;
-    img.className = "card-img-top";
-    img.alt = product.name;
+  let products = await ProductMethod.getAll();
 
-    let cardBody = document.createElement("div");
-    cardBody.className = "card-body";
+  console.log(products);
 
-    let title = document.createElement("h5");
-    title.className = "card-title";
-    title.textContent = product.name;
+  if (SortValue === "lth") {
+    products.sort((a, b) => a.price - b.price);
+  } else if (SortValue === "htl") {
+    products.sort((a, b) => b.price - a.price);
+  }
+  uiMaker(products);
+});
 
-    let desc = document.createElement("p");
-    desc.className = "card-text";
-    desc.textContent = product.decsr;
+//searching
+SearchValue();
 
-    let price = document.createElement("p");
-    price.className = "card-text fw-bold";
-    price.textContent = "₹" + product.price;
+//filter
 
-    let button = document.createElement("button");
-    button.className = "btn btn-primary";
-    button.textContent = "Add to Cart";
-    button.addEventListener("click", async () => {
-      let CartItem = await CartMethod.GetAll();
+document
+  .getElementById("categoryFilter")
+  .addEventListener("change", async (e) => {
+    let FilterValue = e.target.value;
+    console.log(FilterValue);
 
-      let IsExist = CartItem.find((item) => item.id === product.id);
+    let products = await ProductMethod.getAll();
+    console.log(products[0].category);
 
-      if (IsExist) {
-        let upadteitem = { ...IsExist, quantity: IsExist.quantity + 1 };
-        await CartMethod.Update(IsExist.id, upadteitem);
-        alert(`${product.name} has been increase in cart`);
-      } else {
-        let CartAdd = { ...product, quantity: 1 };
-        await CartMethod.Post(CartAdd);
-        console.log("Product added to cart.");
-        alert(`${product.name} added to cart!`);
-      }
-    });
-
-    cardBody.append(title, desc, price, button);
-    div.append(img, cardBody);
-
-    document.getElementById("product-container").append(div);
+    if (FilterValue == "All") {
+      uiMaker(products);
+    } else {
+      let temp = products.filter((ele) => ele.category == FilterValue);
+      uiMaker(temp);
+    }
   });
-};
