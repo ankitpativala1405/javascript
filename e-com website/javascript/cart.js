0eea20b826787564e1e40bc9806cccb3d8979904
Your Name <you@example.com>
2025-04-29 21:21:34 +0530

    cart quantyti update

--- e-com website/javascript/cart.js
+++ e-com website/javascript/cart.js
@@ -19,31 +19,70 @@ isLoggedIn();
 
 const uiMaker = (data) => {
   let subtotal = 0;
-
   document.getElementById("cart-body").innerHTML = "";
 
-  data.map((item) => {
+  data.forEach((item) => {
     const total = item.price * item.quantity;
     subtotal += total;
-  
-    const row = `
-      <tr>
-        <td>${item.name}</td>
-        <td>₹ ${item.price}</td>
-        <td>
-           <button class="decrease-btn" data-id="${item.id}">−</button>
-           <span id="qty-${item.id}">${item.quantity}</span>
-           <button class="increase-btn" data-id="${item.id}">+</button>
-        </td>
-        <td>₹ ${total}</td>
-        <td><button class="remove-btn" data-id="${item.id}">Remove</button></td>
-      </tr>
-    `;
-    document.getElementById("cart-body").innerHTML += row;
+
+    let td1 = document.createElement("td");
+    td1.textContent = item.name;
+
+    let td2 = document.createElement("td");
+    td2.textContent = `₹ ${item.price}`;
+
+    let increase = document.createElement("button");
+    increase.innerHTML = "+";
+    increase.setAttribute("class", "increase-btn");
+    increase.addEventListener("click", async () => {
+      item.quantity += 1;
+      await CartMethod.Update(item.id, { quantity: item.quantity });
+      uiMaker(data);
+    });
+
+    let decrease = document.createElement("button");
+    decrease.innerHTML = "−";
+    decrease.setAttribute("class", "decrease-btn");
+    decrease.addEventListener("click", async() => {
+      item.quantity -= 1;
+      await CartMethod.Update(item.id, { quantity: item.quantity });
+      uiMaker(data);
+    });
+
+    let quantity = document.createElement("span");
+    quantity.id = `qty-${item.id}`;
+    quantity.innerHTML = item.quantity;
+
+    let qtyCell = document.createElement("td");
+    qtyCell.append(decrease, quantity, increase);
+    qtyCell.setAttribute("class", "qtyCell");
+
+    let totalCell = document.createElement("td");
+    totalCell.innerHTML = `₹ ${total}`;
+
+    let removeBtn = document.createElement("button");
+    removeBtn.innerHTML = "Remove";
+    removeBtn.setAttribute("class", "remove-btn");
+    removeBtn.addEventListener("click",async()=>{
+      await CartMethod.Delete(item.id)
+      uiMaker(data)
+    })
+
+    let row = document.createElement("tr");
+    row.append(td1, td2, qtyCell, totalCell, removeBtn);
+
+    document.getElementById("cart-body").appendChild(row);
   });
 
   const totalAmount = subtotal + 100.0;
-  document.getElementById("subtotal").innerText = `Subtotal: ₹ ${subtotal.toFixed(2)}`;
-  document.getElementById("total").innerHTML = `Total: ₹ ${totalAmount.toFixed(2)}`;
+  document.getElementById(
+    "subtotal"
+  ).textContent = `Subtotal: ₹ ${subtotal.toFixed(2)}`;
+  document.getElementById(
+    "total"
+  ).textContent = `Total: ₹ ${totalAmount.toFixed(2)}`;
 };
 
+document.getElementById("WantOrder").addEventListener("click",()=>{
+  alert("ordered...")
+})
